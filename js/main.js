b048016bee8c3f42851643f3d0d8b14bf3ed1c1f
AnnaVerst <89072215+AnnaVerst@users.noreply.github.com>
2022-02-16 16:56:58 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,4 +6,24 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+let burger = document.querySelector('.header-menu-btn');
+let close = document.querySelector('.main-nav-close');
+let menuMobile = document.querySelector('.main-nav')
+
+burger.addEventListener('click', () => {
+  menuMobile.style.display = 'block';
+  burger.style.display = 'none';
+  close.style.display = 'block';
+
+});
+
+close.addEventListener('click', () => {
+  menuMobile.style.display = 'none';
+  close.style.display = 'none';
+  burger.style.display = 'block';
+});
+
+
+
+
 
